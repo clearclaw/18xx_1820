752694d69c2910a6ca231f807a1569b9dbfa3bb3
J C Lawrence <claw@kanga.nu>
2016-10-13 01:07:03 -0700

    privates

--- interest.py
+++ interest.py
@@ -3,7 +3,9 @@
 value = 40
 rate = 1.08
 fix = 0.0021
+old = value
 
 for i in xrange (26):
-  print i, value
+  print i, value, float (value) / float (old)
+  old = value
   value = int ((value * (rate + (i * fix))) + 0.5)
