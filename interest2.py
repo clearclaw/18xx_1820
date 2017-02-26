151b9881fbc0cf92ef1a4e4728b9943919137ddc
J C Lawrence <claw@kanga.nu>
2017-02-26 00:17:27 -0800

    BR rules, no HRs, new stock market, cleanedup papers'

--- interest2.py
+++ interest2.py
@@ -7,7 +7,7 @@ old = value
 
 r = rate - fix
 for i in xrange (32):
-  print "#%2d" % i, "Value: %3d" % value, "Gain: %f" % (float (value) / float (old))
+  print "#%2d" % i, "Value: %3d" % value, "Gain: %f" % (float (value) / float (old)), "Delta: %d" % (value - old)
   old = value
   r += (fix if i <= 13 else - (fix * 1.64))
   value = int ((value * r) + 0.5)
